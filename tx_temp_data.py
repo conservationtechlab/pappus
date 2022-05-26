fbac457042cc8d31044bd764f45e2395089328ac
Trent <trent.moca@gmail.com>
2022-05-26 09:12:52 -0700

    Grouped code into functions to send temperature & time

--- tx_temp_data.py
+++ tx_temp_data.py
@@ -10,6 +10,8 @@ Author: Brent Rubell for Adafruit Industries
 """
 # Import Python System Libraries
 import time
+import subprocess
+
 # Import Blinka Libraries
 import busio
 from digitalio import DigitalInOut, Direction, Pull
@@ -18,81 +20,69 @@ import board
 import adafruit_ssd1306
 # Import RFM9x
 import adafruit_rfm9x
+#For accessing CPU temperature data
+from gpiozero import CPUTemparature
 
-# Button A
-btnA = DigitalInOut(board.D5)
-btnA.direction = Direction.INPUT
-btnA.pull = Pull.UP
 
-# Button B
-btnB = DigitalInOut(board.D6)
-btnB.direction = Direction.INPUT
-btnB.pull = Pull.UP
+def configDisplay():
+    # Create the I2C interface.
+    i2c = busio.I2C(board.SCL, board.SDA)
 
-# Button C
-btnC = DigitalInOut(board.D12)
-btnC.direction = Direction.INPUT
-btnC.pull = Pull.UP
-
-# Create the I2C interface.
-i2c = busio.I2C(board.SCL, board.SDA)
-
-# 128x32 OLED Display
-reset_pin = DigitalInOut(board.D4)
-display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
-# Clear the display.
-display.fill(0)
-display.show()
-width = display.width
-height = display.height
-
-# Configure LoRa Radio
-CS = DigitalInOut(board.CE1)
-RESET = DigitalInOut(board.D25)
-spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
-rfm9x.tx_power = 23
-prev_packet = None
-
-while True:
-    packet = None
-    # draw a box to clear the image
+    # 128x32 OLED Display
+    reset_pin = DigitalInOut(board.D4)
+    display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
+    # Clear the display.
     display.fill(0)
-    display.text('RasPi LoRa', 35, 0, 1)
-
-    # check for packet rx
-    packet = rfm9x.receive()
-    if packet is None:
-        display.show()
-        display.text('- Waiting for PKT -', 15, 20, 1)
-    else:
-        # Display the packet text and rssi
-        display.fill(0)
-        prev_packet = packet
-        packet_text = str(prev_packet, "utf-8")
-        display.text('RX: ', 0, 0, 1)
-        display.text(packet_text, 25, 0, 1)
-        time.sleep(1)
-
-    if not btnA.value:
-        # Send Button A
-        display.fill(0)
-        button_a_data = bytes("Button A!\r\n","utf-8")
-        rfm9x.send(button_a_data)
-        display.text('Sent Button A!', 25, 15, 1)
-    elif not btnB.value:
-        # Send Button B
-        display.fill(0)
-        button_b_data = bytes("Button B!\r\n","utf-8")
-        rfm9x.send(button_b_data)
-        display.text('Sent Button B!', 25, 15, 1)
-    elif not btnC.value:
-        # Send Button C
-        display.fill(0)
-        button_c_data = bytes("Button C!\r\n","utf-8")
-        rfm9x.send(button_c_data)
-        display.text('Sent Button C!', 25, 15, 1)
-
-
     display.show()
-    time.sleep(0.1)
+    return display
+
+def getTemperature():
+    cpu = CPUTemperature()
+    return cpu.temperature #float, in degrees celsius
+
+def configRadio():
+    CS = DigitalInOut(board.CE1)
+    RESET = DigitalInOut(board.D25)
+    spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+    rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
+    rfm9x.tx_power = 23
+    return rfm9x
+
+def sendTemp(rfm9x,count):
+    curr_time = int(time.time())
+    temp_str = str(curr_time) + ": " + str(getTemperature())
+    temp_data = bytes(temp_str,"utf-8")
+    rfm9x.send(temp_data)
+    return temp_str
+
+# returns hostname, first ip, and second ip
+def getHostData(): #derived from scrubcam "little_readout.py"
+    cmd = "hostname"
+    hostname = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = "hostname -I | cut -d' ' -f1"
+    ip1 = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = "hostname -I | cut -d' ' -f2"
+    ip2 = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    return hostname, ip1, ip2
+
+if __name__ == '__main__':
+    rfm9x = configRadio()
+    
+    display = configDisplay()
+    spacing = int(display.height/4)
+
+    hostname, ip1, ip2 = getHostData()
+    host_text = "Host: " + hostname
+    ip1_text = "IP1: " + ip1
+    ip2_text = "IP2: " + ip2
+    
+    while(True):
+        display.fill(0)
+        sent_packet = sendTemp(rfm9x)
+        time.sleep(1)
+        display.show()
+        display.text(host_text, 0,0,1)
+        display.text(ip1_text, 0,spacing,1)
+        display.text(ip2_text,0,2*spacing,1)
+        display.text(sent_packet,0,3*spacing,1)
+        display.show()
